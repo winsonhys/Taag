8d7e01474855055ed6f9f832eb744389591958bb
winsonhys <winsonheng@hotmail.com>
2020-07-31 23:31:53 +0800

    Changed package name and app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:Taag/Home/home.dart';
@@ -5,8 +6,9 @@ import 'package:Taag/ItemDetails/ItemDetails.dart';
 import 'package:Taag/Login/screens/LoginScreen.dart';
 import 'package:Taag/graphql/GraphqlContainer.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(GraphQLContainer(
     child: MyApp(),
   ));
